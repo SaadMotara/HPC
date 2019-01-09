3355e4316f1c47a6af0e185998f7ff9537f6d3ad
SaadMotara <37440261+SaadMotara@users.noreply.github.com>
2019-01-09 15:45:12 +0000

    3 letters 2 number
    added a new locounter s to run the program with 3 letters and genrated 3 password to test it

---
+++ POSIX Threads/Password AAA11(3letters to numbers)/CrackAZ99-With-Data.c
@@ -0,0 +1,115 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <crypt.h>
+#include <time.h>
+#include <math.h>
+
+/******************************************************************************
+  Demonstrates how to crack an encrypted password using a simple
+  "brute force" algorithm. Works on passwords that consist only of 2 uppercase
+  letters and a 2 digit integer. Your personalised data set is included in the
+  code.
+
+  Compile with:
+    cc -o CrackAZ99-With-Data CrackAZ99-With-Data.c -lcrypt
+
+  If you want to analyse the results then use the redirection operator to send
+  output to a file that you can view using an editor or the less utility:
+
+    ./CrackAZ99-With-Data > results.txt
+
+  Dr Kevan Buckley, University of Wolverhampton, 2018
+******************************************************************************/
+int n_passwords = 3;
+
+char *encrypted_passwords[] = {
+  //"$6$KB$yPvAP5BWyF7oqOITVloN9mCAVdel.65miUZrEel72LcJy2KQDuYE6xccHS2ycoxqXDzW.lvbtDU5HuZ733K0X0",
+  //"$6$KB$4dWyDaT7p8xMWfb6.1R4Q7hQMyezwOgLfAcZhdE4QkH3bylH4EmM.gOVhU7m7K9FHfL.kKhzK6bfHqfA.NDb/0",
+  //"$6$KB$xELPxo7gwBciT/5FN/.kBwgSPG5RZ8TiW8N0hEER1gyYrQ9ErvSfTbGuDl4w9/QyIP5Lu6W25.G.C8jk6VTYL0",
+  //"$6$KB$YsfzwjTmZTAEUfD/L/uEqVAmfKGAWSa0ZWg8tSPGT4DZnQNBzLT4vXg2nl7Yd2KZrs4dykykxVIZZLob5ijUb/"
+  "$6$KB$mczNx5tYSqyEwxnZNEfhZ6NMdfjGHyHfLnOu/kKwxC1RE59K/o74x8rCRFX6vXuCDyhLGBsFvk.grhZLEBa9q1",
+  "$6$KB$P0iw4e1VDQn.W98MT1amNIPSwFjf4H.GFmSzdQKBFGoK8qZIqvaSIPXDB6wcCNK3KJak14qC1qntCm0nJxubJ1",
+  "$6$KB$p.viTy0M23kTxeFXNDL6gWrT4JmMi8o6X2hyoQpknf5ZvALWtUmK93Tw7LOtHWveUnbY6nJrmGZ7lfX7VY21b0/"
+//theses 3 password should be AAA11, BBB22, CCC33
+};
+
+/**
+ Required by lack of standard function in C.  
+*/
+
+void substr(char *dest, char *src, int start, int length){
+  memcpy(dest, src + start, length);
+  *(dest + length) = '\0';
+}
+
+/**
+ This function can crack the kind of password explained above. All combinations
+ that are tried are displayed and when the password is found, #, is put at the
+ start of the line. Note that one of the most time consuming operations that
+ it performs is the output of intermediate results, so performance experiments
+ for this kind of program should not include this. i.e. comment out the printfs.
+*/
+
+void crack(char *salt_and_encrypted){
+  int x, y, z, s;     // Loop counters
+  char salt[7];    // String used in hashing the password. Need space for \0
+  char plain[7];   // The combination of letters currently being checked
+  char *enc;       // Pointer to the encrypted password
+  int count = 0;   // The number of combinations explored so far
+
+  substr(salt, salt_and_encrypted, 0, 6);
+
+  for(x='A'; x<='Z'; x++){
+    for(y='A'; y<='Z'; y++){
+	for(s='A'; s<='Z'; s++){
+      	for(z=0; z<=99; z++){
+        sprintf(plain, "%c%c%c%02d", x, y, s, z);
+        enc = (char *) crypt(plain, salt);
+        count++;
+        if(strcmp(salt_and_encrypted, enc) == 0){
+          printf("#%-8d%s %s\n", count, plain, enc);
+        } else {
+          //printf(" %-8d%s %s\n", count, plain, enc);
+        }
+      }
+    }
+  }
+}
+  printf("%d solutions explored\n", count);
+}
+
+int time_difference(struct timespec *start, struct timespec *finish, 
+                    long long int *difference) {
+  long long int ds =  finish->tv_sec - start->tv_sec; 
+  long long int dn =  finish->tv_nsec - start->tv_nsec; 
+
+  if(dn < 0 ) {
+    ds--;
+    dn += 1000000000; 
+  } 
+  *difference = ds * 1000000000 + dn;
+  return !(*difference > 0);
+}
+
+int main(int argc, char *argv[]){
+	struct timespec start, finish;   
+  long long int time_elapsed;
+
+  	clock_gettime(CLOCK_MONOTONIC, &start);
+
+  int i;
+ 
+  for(i=0;i<n_passwords;i<i++) {
+    crack(encrypted_passwords[i]);
+  }
+
+	clock_gettime(CLOCK_MONOTONIC, &finish);
+  time_difference(&start, &finish, &time_elapsed);
+  printf("Time elapsed was %lldns or %0.9lfs\n", time_elapsed, 
+         (time_elapsed/1.0e9)); 
+
+  return 0;
+}
+
+
